0a41403004a9d0939d19c0a01000e21246b33d26
Orlov Vlad <vladorlov3333@gmail.com>
2022-12-04 13:43:59 +0200

    commitJS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,175 @@
 let moviesList = null;
+let inputSearch = null;
+
+const createElement = ({
+  type,
+  attrs,
+  container = null,
+  position = 'append',
+}) => {
+  const el = document.createElement(type);
+
+  Object.keys(attrs).forEach((key) => {
+    if (key !== 'innerHTML') el.setAttribute(key, attrs[key]);
+    else el.innerHTML = attrs[key];
+  });
+
+  if (container && position === 'append') container.append(el);
+  if (container && position === 'prepend') container.prepend(el);
+
+  return el;
+};
+
+const createStyle = () => {
+  const style = document.createElement('style');
+
+  style.innerHTML = `
+  * {
+  box-sizing: border-box;
+ }
+
+body {
+  margin: 0;
+}
+
+.container {
+  padding: 20px;
+}
+
+.movies {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 20px;
+}
+
+.movie {
+  display: flex;
+  align-content: center;
+  justify-content: center;
+}
+
+.movie__image {
+  width: 100%;
+}
+`;
+
+  document.head.append(style);
+};
 
 const createMarkup = () => {
-  const container = document.createElement('div');
-  const movies = document.createElement('div');
+  const container = createElement({
+    type: 'div',
+    attrs: { class: 'container' },
+    container: document.body,
+    position: 'prepend',
+  });
 
-  container.setAttribute('class', 'container');
-  movies.setAttribute('class', 'movies');
+  createElement({
+    type: 'h1',
+    attrs: { innerHTML: 'Приложения для поиска фильмов' },
+    container,
+  });
 
-  container.append(movies);
-  document.body.prepend(container);
+  // header.innerHTML = 'Приложения для поиска фильмов';
+  // container.append(header);
 
-  moviesList = document.querySelector('.movies');
+  const searchBox = createElement({
+    type: 'div',
+    attrs: { class: 'search' },
+    container,
+  });
+
+  const inputBox = createElement({
+    type: 'div',
+    attrs: { class: 'search__group search__group--input' },
+    container: searchBox,
+  });
+
+  const checkBox = createElement({
+    type: 'div',
+    attrs: { class: 'search__group search__group--checkbox' },
+    container: searchBox,
+  });
+
+  createElement({
+    type: 'label',
+    attrs: {
+      for: 'search',
+      innerHTML: 'Поиск фильмов',
+    },
+    container: inputBox,
+  });
+
+  inputSearch = createElement({
+    type: 'input',
+    attrs: {
+      class: 'search__input',
+      id: 'search',
+      type: 'search',
+      placeholder: 'Начните вводить текст...',
+    },
+    container: inputBox,
+  });
+
+  createElement({
+    type: 'input',
+    attrs: {
+      class: 'search__checkbox',
+      id: 'checkbox',
+      type: 'checkbox',
+    },
+    container: checkBox,
+  });
+
+  createElement({
+    type: 'label',
+    attrs: {
+      for: 'checkbox',
+      innerHTML: 'Добавлять фильмы к уже найденным',
+    },
+    container: checkBox,
+  });
+
+  // <h1>Приложения для поиска фильмов</h1>
+  // <div class="search">
+  //   <div class="search__group search__group--input">
+  //     <label for="">Поиск фильмов</label>
+  //     <input id="search" type="search" placeholder="Начните вводить текст...">
+  //   </div>
+
+  //   <div class="search__group search__group--checkbox">
+  //     <input id="checkbox" type="checkbox" placeholder="">
+  //     <label for="">Добавлять фильмы к уже найденным</label>
+  //   </div>
+  // </div>
+
+  moviesList = createElement({
+    type: 'div',
+    attrs: { class: 'movies' },
+    container,
+  });
+
+  console.log(moviesList);
 };
 
 const addMovieToList = (movie) => {
-  const item = document.createElement('div');
-  const img = document.createElement('img');
-
-  item.setAttribute('class', 'movie');
-  img.setAttribute('class', 'movie__image');
-
-  if (/^(http|https):\/\//i.test(movie.Poster)) {
-    img.src = movie.Poster;
-  } else {
-    img.src = 'assets/img/no-image.png';
-  }
-
-  img.alt = `${movie.Title}, ${movie.Year}`;
-  img.title = `${movie.Title}, ${movie.Year}`;
-
-  item.append(img);
-  moviesList.append(item);
+  const item = createElement({
+    type: 'div',
+    attrs: { class: 'movie' },
+    container: moviesList,
+  });
+  createElement({
+    type: 'img',
+    attrs: {
+      class: 'movie__image',
+      src: /^(http|https):\/\//i.test(movie.Poster)
+        ? movie.Poster
+        : 'assets/img/no-image.png',
+      alt: `${movie.Title}, ${movie.Year}`,
+      title: `${movie.Title}, ${movie.Year}`,
+    },
+    container: item,
+  });
 };
 
 const getData = (url) =>
@@ -41,6 +180,7 @@ const getData = (url) =>
 const search = 'ironman';
 
 createMarkup();
+createStyle();
 
 getData(`http://www.omdbapi.com/?apikey=18b8609f&s=${search}`)
   .then((movies) => movies.forEach((movie) => addMovieToList(movie)))
