c3e9d9c9bd26e74cdffe5ca6334396cd772f7e36
Orlov Vlad <vladorlov3333@gmail.com>
2022-12-06 13:10:56 +0200

    lastCommit

--- assets/js/dom.js
+++ assets/js/dom.js
@@ -1,11 +1,14 @@
-let moviesList = null;
-let inputSearch = null;
+export let moviesList = null;
+export let inputSearch = null;
+export let triggerMode = false;
 
 const createElement = ({
   type,
   attrs,
   container = null,
   position = 'append',
+  evt = null,
+  handler = null,
 }) => {
   const el = document.createElement(type);
 
@@ -16,47 +19,70 @@ const createElement = ({
 
   if (container && position === 'append') container.append(el);
   if (container && position === 'prepend') container.prepend(el);
+  if (evt && handler && typeof handler === 'function')
+    el.addEventListener(evt, handler);
 
   return el;
 };
 
-const createStyle = () => {
-  const style = document.createElement('style');
-
-  style.innerHTML = `
-  * {
-  box-sizing: border-box;
- }
-
-body {
-  margin: 0;
-}
-
-.container {
-  padding: 20px;
-}
-
-.movies {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  gap: 20px;
-}
-
-.movie {
-  display: flex;
-  align-content: center;
-  justify-content: center;
-}
-
-.movie__image {
-  width: 100%;
-}
-`;
-
-  document.head.append(style);
+export const createStyle = () => {
+  createElement({
+    type: 'style',
+    attrs: {
+      innerHTML: `
+      * {
+        box-sizing: border-box;
+      }
+      body {
+        margin: 0;
+        font-family: Arial, Helvetica, sans-serif;
+      }
+      .container {
+        padding: 20px;
+        max-width: 1280px;
+        margin: 0 auto;
+      }
+      .movies {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+        gap: 20px;
+      }
+      .movie {
+        display: flex;
+        align-content: center;
+        justify-content: center;
+      }
+      .movie__image {
+        width: 100%;
+        object-fit: cover;
+      }
+      .search {
+        margin-bottom: 30px;
+      }
+      .search__input {
+        display: block;
+        max-width: 400px;
+        width: 100%;
+        padding: 10px 15px;
+        margin-bottom: 10px;
+        border-radius: 4px;
+        border: 1px solid lightsteelblue;
+      }
+      .search__label-input {
+        display: block;
+        margin-bottom: 7px;
+      }
+      .search__label-checkbox {
+        font-size: 12px;
+        display: inline-block;
+        transform: translate(7px, -2px);
+      }`,
+    },
+    container: document.head,
+  });
 };
 
-const createMarkup = () => {
+export const createMarkup = () => {
   const container = createElement({
     type: 'div',
     attrs: { class: 'container' },
@@ -69,9 +95,6 @@ const createMarkup = () => {
     attrs: { innerHTML: 'Приложения для поиска фильмов' },
     container,
   });
-
-  // header.innerHTML = 'Приложения для поиска фильмов';
-  // container.append(header);
 
   const searchBox = createElement({
     type: 'div',
@@ -95,6 +118,7 @@ const createMarkup = () => {
     type: 'label',
     attrs: {
       for: 'search',
+      class: 'search__label-input',
       innerHTML: 'Поиск фильмов',
     },
     container: inputBox,
@@ -119,40 +143,28 @@ const createMarkup = () => {
       type: 'checkbox',
     },
     container: checkBox,
+    evt: 'click',
+    handler: () => (triggerMode = !triggerMode),
   });
 
   createElement({
     type: 'label',
     attrs: {
       for: 'checkbox',
+      class: 'search__label-checkbox',
       innerHTML: 'Добавлять фильмы к уже найденным',
     },
     container: checkBox,
   });
-
-  // <h1>Приложения для поиска фильмов</h1>
-  // <div class="search">
-  //   <div class="search__group search__group--input">
-  //     <label for="">Поиск фильмов</label>
-  //     <input id="search" type="search" placeholder="Начните вводить текст...">
-  //   </div>
-
-  //   <div class="search__group search__group--checkbox">
-  //     <input id="checkbox" type="checkbox" placeholder="">
-  //     <label for="">Добавлять фильмы к уже найденным</label>
-  //   </div>
-  // </div>
 
   moviesList = createElement({
     type: 'div',
     attrs: { class: 'movies' },
     container,
   });
-
-  console.log(moviesList);
 };
 
-const addMovieToList = (movie) => {
+export const addMovieToList = (movie) => {
   const item = createElement({
     type: 'div',
     attrs: { class: 'movie' },
@@ -172,16 +184,4 @@ const addMovieToList = (movie) => {
   });
 };
 
-const getData = (url) =>
-  fetch(url)
-    .then((response) => response.json())
-    .then((json) => json.Search);
-
-const search = 'ironman';
-
-createMarkup();
-createStyle();
-
-getData(`http://www.omdbapi.com/?apikey=18b8609f&s=${search}`)
-  .then((movies) => movies.forEach((movie) => addMovieToList(movie)))
-  .catch((err) => console.log(err));
+export const clearMoviesMarkup = (el) => el && (el.innerHTML = '');
